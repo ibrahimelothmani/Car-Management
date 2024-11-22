8f5af999c724fbc2ddd508c92b6ea8c27e1e2abd
Ibrahim El Othmani <ibrahimelothmani98@gmail.com>
2024-11-22 15:35:54 +0100

    Create the components , the service and import the modules in the AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CarsModule } from './cars/cars.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarsModule,
+    SharedModule
   ],
   providers: [
     provideClientHydration()
