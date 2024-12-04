4bac3503e95771e31f054d348f397902b7cf83a4
Ibrahim El Othmani <ibrahimelothmani98@gmail.com>
2024-12-04 22:34:53 +0100

    create the components, the service, the guard and finally install the keycloak and create his service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddAdminComponent } from './add-admin/add-admin.component';
+import { ModifyAdminComponent } from './modify-admin/modify-admin.component';
+import { ListClientComponent } from './list-client/list-client.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
+import { AddCarComponent } from './add-car/add-car.component';
+import { ModifyCarComponent } from './modify-car/modify-car.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ListAdminComponent } from './list-admin/list-admin.component';
+import { ListCarComponent } from './list-car/list-car.component';
+import { AuthGuard } from './service/authguard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'addadmin',
+    component: AddAdminComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['admin'] }, // Requires 'admin' role
+  },
+  {
+    path: 'modify',
+    component: ModifyAdminComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['admin', 'editor'] }, // Requires either 'admin' or 'editor' role
+  },
+  { path: 'admin', component: ListAdminComponent, canActivate: [AuthGuard] },
+  {
+    path: 'client',
+    component: ListClientComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['client-manager'] }, // Requires 'client-manager' role
+  },
+  {
+    path: 'contact',
+    component: ListContactComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'car', component: ListCarComponent, canActivate: [AuthGuard] },
+  {
+    path: 'addcar',
+    component: AddCarComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['car-manager'] }, // Requires 'car-manager' role
+  },
+  {
+    path: 'modifycar',
+    component: ModifyCarComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddAdminComponent } from './add-admin/add-admin.component';
+import { AddCarComponent } from './add-car/add-car.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ListClientComponent } from './list-client/list-client.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
+import { ListAdminComponent } from './list-admin/list-admin.component';
+import { ListCarComponent } from './list-car/list-car.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { ModifyAdminComponent } from './modify-admin/modify-admin.component';
+import { ModifyCarComponent } from './modify-car/modify-car.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAdminComponent,
+    AddCarComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ListClientComponent,
+    ListContactComponent,
+    ListAdminComponent,
+    ListCarComponent,
+    LoginComponent,
+    MenuComponent,
+    ModifyAdminComponent,
+    ModifyCarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
