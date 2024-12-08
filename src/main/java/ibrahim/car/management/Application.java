732ea4c72833dfac18a9a1bcaefe8fb0e8c38563
Ibrahim El Othmani <ibrahimelothmani98@gmail.com>
2024-12-08 21:40:11 +0100

    enable security in our started app  => @EnableWebSecurity && configuration of the compose.yaml

--- src/main/java/ibrahim/car/management/Application.java
+++ src/main/java/ibrahim/car/management/Application.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class Application {
 private static final Logger Log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
